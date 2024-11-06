605b1f63c3e8a2a17fb89b0110b9464f2af00ba1
Văn Thắng <vanthang23032001@gmail.com>
2024-11-06 15:29:25 +0700

    initial commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.26",
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
 
 export default config;
